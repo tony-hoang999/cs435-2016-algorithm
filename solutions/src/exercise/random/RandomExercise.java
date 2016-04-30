89e745d0b27c4c7a742f4e034c05ff934fd02cfe
Long Huynh <huynhquanglong@gmail.com>
2016-04-29 23:16:09 -0500

    random excercise

--- solutions/src/exercise/random/RandomExercise.java
+++ solutions/src/exercise/random/RandomExercise.java
@@ -1,6 +1,7 @@
 package exercise.random;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class RandomExercise {
@@ -8,26 +9,23 @@ public class RandomExercise {
 	public static void main(String[] args) {
 		randomNumber(4);
 	}
-	
-	private static void randomNumber(int n){
-        ArrayList<Integer> list = new ArrayList<Integer>(n);
-        for(int i = 1; i <= n; i++) {
-            list.add(i);
-        }
-        
-        int half = 0;
-        
-        Random random = new Random();
-        while(list.size() > 0) {
-        	half++;
-            int index = random.nextInt(list.size());            
-            System.out.println(list.remove(index));
-            if(half == n/2)
-            	System.out.println("============");
-           
-        }
-	}
-	
-	
 
+	private static void randomNumber(int n) {
+		List<Integer> list = new ArrayList<Integer>(n);
+		for (int i = 1; i <= n; i++) {
+			list.add(i);
+		}
+
+		int half = 0;
+
+		Random random = new Random();
+		while (list.size() > 0) {
+			half++;
+			int index = random.nextInt(list.size());
+			System.out.println(list.remove(index));
+			if (half == n / 2)
+				System.out.println("============");
+
+		}
+	}
 }
